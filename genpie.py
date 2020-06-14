10f7313c1ab89decf068704e7bce2815466853ee
chiamyunqing <chiamyunqing@gmail.com>
2020-06-14 16:36:42 +0800

    Test for pie chart feature with matploblib (need to fix error message in server and wrong pie chart for multiple users)

--- genpie.py
+++ genpie.py
@@ -1,14 +1,11 @@
-import pygal
+import matplotlib.pyplot as plt
 
-chat_id = "ID USED FOR TESTING"
-curr_food_exp = dbhelper.get_monthly_category_exp(chat_id, year_month, 'FOOD')
-curr_clothes_exp = dbhelper.get_monthly_category_exp(chat_id, year_month, 'CLOTHES')
-curr_transport_exp = dbhelper.get_monthly_category_exp(chat_id, year_month, 'TRANSPORT')
-curr_necc_exp = dbhelper.get_monthly_category_exp(chat_id, year_month, 'NECESSITIES')
-curr_others_exp = dbhelper.get_monthly_category_exp(chat_id, year_month, 'OTHERS')
-pie_chart = pygal.Pie()
-pie_chart.title = 'Current monthly expenditure (in %)'
-results = [(convert_str(curr_food_exp),'FOOD'), (convert_str(curr_clothes_exp),'CLOTHES'), (convert_str(curr_transport_exp),'TRANSPORT'), (convert_str(curr_necc_exp),'NECESSITIES'), (convert_str(curr_others_exp),'OTHERS')]
-for r in results:
-    pie_chart.add(r[1], [{'value': r[0], 'label': r[1]}])
-    pie_chart.render_tree() 
+class chart:
+    def make_pie(self, f, c, t, d, o):
+        print("done")
+        # Pie chart, where the slices will be ordered and plotted counter-clockwise:
+        sizes = [f, c, t, d, o]
+        explode = (0, 0, 0, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+        plt.pie(sizes, explode=explode, labels=labels)
+        plt.savefig('foo.png')
+
