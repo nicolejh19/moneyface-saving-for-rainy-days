5eb3e507e69f5eccbbb763270c464cce915a0097
nic jh <65008375+nicolejh19@users.noreply.github.com>
2020-06-20 21:56:21 +0800

    Modify table and function for Challenge Friend in Socialite feature

--- databasehelper.py
+++ databasehelper.py
@@ -20,7 +20,7 @@ class DBHelper:
         dbcursor.execute('''CREATE TABLE IF NOT EXISTS monthly_savings (date_time DATE NOT NULL, year_month INTEGER NOT NULL, user_id INTEGER NOT NULL, amount TEXT NOT NULL, type_savings TEXT NOT NULL, PRIMARY KEY(date_time, user_id), FOREIGN KEY(user_id) REFERENCES users(user_id))''')
         dbcursor.execute('''CREATE TABLE IF NOT EXISTS iou (user_id_debtor INTEGER NOT NULL, name_debtee TEXT NOT NULL, amount TEXT NOT NULL, PRIMARY KEY(user_id_debtor, name_debtee), FOREIGN KEY(user_id_debtor) REFERENCES users(user_id))''')
         dbcursor.execute('''CREATE TABLE IF NOT EXISTS uome (user_id_debtee INTEGER NOT NULL, name_debtor TEXT NOT NULL, amount TEXT NOT NULL, PRIMARY KEY(user_id_debtee, name_debtor), FOREIGN KEY(user_id_debtee) REFERENCES users(user_id))''')
-        dbcursor.execute('''CREATE TABLE IF NOT EXISTS challenges (date_time DATE NOT NULL, year_month INTEGER NOT NULL, user_id_challenger INTEGER NOT NULL, user_id_challenged INTEGER NOT NULL, amount TEXT NOT NULL, PRIMARY KEY(date_time, user_id_challenger, user_id_challenged), FOREIGN KEY(user_id_challenged) REFERENCES users(user_id), FOREIGN KEY(year_month, user_id_challenged) REFERENCES monthly_exp(year_month, user_id))''')
+        dbcursor.execute('''CREATE TABLE IF NOT EXISTS challenges (year_month INTEGER NOT NULL, user_id_challenger INTEGER NOT NULL, user_id_challenged INTEGER NOT NULL, amount TEXT NOT NULL, PRIMARY KEY(year_month, user_id_challenger, user_id_challenged), FOREIGN KEY(user_id_challenged) REFERENCES users(user_id), FOREIGN KEY(year_month, user_id_challenged) REFERENCES monthly_exp(year_month, user_id))''')
         db.commit()
         dbcursor.close()
 
@@ -583,15 +583,20 @@ class DBHelper:
         finally:
             dbcursor.close() 
 
-    def add_challenge(self, date_time, year_month, user_id_challenger, user_id_challenged, amount):
+    def add_challenge(self, year_month, user_id_challenger, user_id_challenged, amount):
         db = sqlite3.connect(self.dbname)
         dbcursor = db.cursor()
-        stmt = '''INSERT INTO challenges (date_time, year_month, user_id_challenger, user_id_challenged, amount) VALUES (?, ?, ?, ?, ?)'''
-        args = (date_time, year_month, user_id_challenger, user_id_challenged, amount, )
-        dbcursor.execute(stmt, args)
-        db.commit()
-        dbcursor.close()
-        
+        stmt = '''INSERT INTO challenges (year_month, user_id_challenger, user_id_challenged, amount) VALUES (?, ?, ?, ?, ?)'''
+        args = (year_month, user_id_challenger, user_id_challenged, amount, )
+        try:
+            dbcursor.execute(stmt, args)
+            db.commit()
+            return True
+        except sqlite3.IntegrityError:
+            return False
+        finally:
+            dbcursor.close()
+       
     def get_challenge_amount(self, user_id_challenger, user_id_challenged, year_month):
         db = sqlite3.connect(self.dbname)
         dbcursor = db.cursor()
