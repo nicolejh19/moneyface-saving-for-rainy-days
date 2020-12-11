e34bcf583d0bd9adfa6612426eb068e463be7cda
chiamyunqing <60507245+chiamyunqing@users.noreply.github.com>
2020-12-11 16:18:50 +0800

    Create poll.js

---
+++ docs/poll.js
@@ -0,0 +1,13 @@
+//let yes = 0;
+//let no = 0;
+function poll(ans) {
+    if (ans === 'Yes') {
+        alert("Yay! We are glad that you enjoy using the bot! :D");
+        //yes += 1;
+       // alert(`${yes} others liked our bot too! :D`);
+    } else {
+        alert("Oh dear... let us know how we can improve further! :("); 
+       // no += 1;
+       // alert(`${no} others did not enjoy using our bot :(`);
+    }
+}
